60073746dbc5480d0bd42f75769c7639a8ab809d
pkiraly <pkiraly@gwdg.de>
2021-06-02 22:14:07 +0200

    Initial commit

---
+++ src/test/java/de/gwdg/metadataqa/marc/ws/AppTest.java
@@ -0,0 +1,20 @@
+package de.gwdg.metadataqa.marc.ws;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
