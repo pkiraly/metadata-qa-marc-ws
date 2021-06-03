f4fb5c7956e2e24d3a1106de9cdbbe42bcb5ae4f
pkiraly <pkiraly@gwdg.de>
2021-06-03 15:37:28 +0200

    Setting up a Spring structure.

--- src/main/java/de/gwdg/metadataqa/marc/ws/App.java
+++ src/main/java/de/gwdg/metadataqa/marc/ws/App.java
@@ -1,13 +1,27 @@
 package de.gwdg.metadataqa.marc.ws;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(
+        @RequestParam(value = "myName", defaultValue = "World") String name
+    ) {
+        return String.format("Hello %s!", name);
     }
 }
