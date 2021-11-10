a8326a50a02881121624da2f16d21d054cc744c4
pkiraly <pkiraly@gwdg.de>
2021-11-10 21:23:12 +0100

    Prepare for Tomcat deploy

--- src/main/java/de/gwdg/metadataqa/marc/ws/App.java
+++ src/main/java/de/gwdg/metadataqa/marc/ws/App.java
@@ -1,38 +1,20 @@
 package de.gwdg.metadataqa.marc.ws;
 
-import de.gwdg.metadataqa.marc.cli.Validator;
-import de.gwdg.metadataqa.marc.cli.parameters.ValidatorParameters;
-import de.gwdg.metadataqa.marc.cli.utils.RecordIterator;
-import de.gwdg.metadataqa.marc.definition.DataSource;
-import de.gwdg.metadataqa.marc.model.validation.ValidationError;
-import de.gwdg.metadataqa.marc.model.validation.ValidationErrorFormatter;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * QA Catalogue web service
  */
 @SpringBootApplication
 @RestController
-public class App {
+public class App extends SpringBootServletInitializer {
 
     @Value("${spring.application.name}")
     String appName;
